e74f01669e1555b93947fa6b2d31d911842cc37e
David Leal <davidvexel@gmail.com>
2020-06-17 14:24:09 -0500

    Adding modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,29 @@
-document.getElementById("openPopup").onclick = function () {
-    if (document.getElementById("popup").style.display == "block") {
-        document.getElementById("popup").style.display = "none";
-    } else {
-        document.getElementById("popup").style.display = "block";
-    }
-}
+// Modal
+const modalTriggers = document.querySelectorAll('.popup-trigger');
+const bodyBlackout = document.querySelector('.body-blackout');
 
+modalTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const { popupTrigger } = trigger.dataset;
+        const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`);
+
+        popupModal.classList.add('is--visible');
+        bodyBlackout.classList.add('is-blacked-out');
+
+        bodyBlackout.addEventListener('click', () => {
+            // TODO: Turn into a function to close modal
+            popupModal.classList.remove('is--visible');
+            bodyBlackout.classList.remove('is-blacked-out');
+        })
+    })
+})
+
+
+// Datepicker
 $("#date").datepicker();
 
-document.getElementById("submit").onclick = function () {
-
-
-}
+// Submit
+document.getElementById("submit").onclick = function () {}
 
 /* Hamburger Menu */
 document.getElementById("openMenu").onclick = function () {
